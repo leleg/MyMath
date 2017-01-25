ac7ce997385a5e5cae31747f4560b3e0fc758753
Gaurav Lele <lelegaurav@gmail.com>
2017-01-25 12:50:20 +0530

    First commit

---
+++ MyMath/src/Test/Main.java
@@ -0,0 +1,13 @@
+package Test;
+import mymath.MyMath;
+
+
+public class Main {
+	
+	public static void main(String[] args){
+		MyMath mymath = new MyMath();
+		System.out.println("Result: " + Math.multiplyExact(5, 4));
+		
+	}
+
+}
---
+++ MyMath/src/mymath/MyMath.java
@@ -0,0 +1,8 @@
+package mymath;
+
+public class MyMath {
+
+	public int multi(int num1, int num2){
+		return num1*num2;
+	}
+}
